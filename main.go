1b1f2ac1a8eb2c75d99d976e08981e05348a3b00
Robson Porto <robbporto@gmail.com>
2020-11-17 22:36:40 -0800

    Add comment

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	//var card = "Ace of Spades"
+	//var card = "Ace of Spades" // Variables need to be initialized before being assigned
 	card := "Ace of Spades" // Only ":=" when defining a new variable
 	card = "Five of Diamonds"
 
