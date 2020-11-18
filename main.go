ee2fc45ed6ea6fbfe27b35638db18f542404d18c
Robson Porto <robbporto@gmail.com>
2020-11-17 22:31:39 -0800

    Lesson 14 - Variable Declaration

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card = "Ace of Spades"
+	card := "Ace of Spades" // Only ":=" when defining a new variable
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
